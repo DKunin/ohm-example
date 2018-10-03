ace21621697f969eb3db9fc6b870295ad611afb1
Dmitry Kunin <dkunin@avito.ru>
2018-10-03 09:37:12 +0300

    Update

--- remark/parser.js
+++ remark/parser.js
@@ -20,59 +20,60 @@ RemarkJSSlider.grammar = ohm.grammar(`
   }
 `);
 
-RemarkJSSlider.semantics = RemarkJSSlider.grammar.createSemantics()
-.addOperation('toAST', {
-  Rules(rules) {
-    return new Program(rules.toAST());
-  },
+RemarkJSSlider.semantics = RemarkJSSlider.grammar
+    .createSemantics()
+    .addOperation('toAST', {
+        Rules(rules) {
+            return new Program(rules.toAST());
+        },
 
-  Rule_fact(head) {
-    return new Rule(head.toAST(), []);
-  },
+        Rule_fact(head) {
+            return new Rule(head.toAST(), []);
+        },
 
-  Clause(parts) {
-    let name = '';
-    const args = [];
-    parts.children.forEach((part, idx) => {
-      if (part.isA('word') ) {
-        if (idx > 0 && part.sourceString[0] !== "'") {
-          name = name + ' ';
+        Clause(parts) {
+            let name = '';
+            const args = [];
+            parts.children.forEach((part, idx) => {
+                if (part.isA('word')) {
+                    if (idx > 0 && part.sourceString[0] !== "'") {
+                        name = name + ' ';
+                    }
+                    name = name + part.sourceString;
+                } else {
+                    if (idx > 0) {
+                        name = name + ' ';
+                    }
+                    name = name + '@';
+                    args.push(part.toAST());
+                }
+            });
+            return new Clause(name, args);
+        },
+
+        classRow(row, classNames, a, b) {
+            return new ClassRow(row.sourceString, classNames.sourceString);
+        },
+
+        EmptyListOf() {
+            return [];
+        },
+
+        NonemptyListOf(x, sep, xs) {
+            return [x.toAST()].concat(xs.toAST());
         }
-        name = name + part.sourceString;
-      } else {
-        if (idx > 0) {
-          name = name + ' ';
+    })
+    .addOperation('isA(type)', {
+        _nonterminal(children) {
+            return this.ctorName === this.args.type;
         }
-        name = name + '@';
-        args.push(part.toAST());
-      }
     });
-    return new Clause(name, args);
-  },
-
-  classRow(row, classNames, a, b) {
-    return new ClassRow(row.sourceString, classNames.sourceString);
-  },
-
-  EmptyListOf() {
-    return [];
-  },
-
-  NonemptyListOf(x, sep, xs) {
-    return [x.toAST()].concat(xs.toAST());
-  }
-
-}).addOperation('isA(type)', {
-  _nonterminal(children) {
-    return this.ctorName === this.args.type;
-  }
-});
 
 RemarkJSSlider.parse = function(input) {
-  const matchResult = this.grammar.match(input);
-  if (matchResult.succeeded()) {
-    return this.semantics(matchResult).toAST();
-  } else {
-    throw new Error(matchResult.message);
-  }
+    const matchResult = this.grammar.match(input);
+    if (matchResult.succeeded()) {
+        return this.semantics(matchResult).toAST();
+    } else {
+        throw new Error(matchResult.message);
+    }
 };
